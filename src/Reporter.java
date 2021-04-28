ccc57b1c9fc3d1c46859f59d7e4fb5ccd3cd6dce
Navid Nafiuzzaman <mxn4459@rit.edu>
2021-04-27 23:26:21 -0400

    Some error handling done, gotta figure out OAEP->encode

--- src/Reporter.java
+++ src/Reporter.java
@@ -1,9 +1,69 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.util.Scanner;
+
 public class Reporter {
-    public static void main(String[] args) {
-        String rHost = args[0];
-        int rPort = Integer.parseInt(args[1]);
-//        FileInputStream
+    public static void main(String[] args) throws SocketException {
         if (args.length < 3 ) argsError();
+
+        String rHost = args[0];
+
+        int rPort = Integer.parseInt(args[1]);
+        DatagramSocket mailbox = null;
+
+
+        try{
+            mailbox = new DatagramSocket(new InetSocketAddress(rHost, rPort));
+        }catch (SocketException e){
+            e.printStackTrace();
+            System.err.printf("Error in accessing the specific address %s:%d", rHost, rPort);
+            argsError();
+            System.exit(1);
+        }
+
+
+        Scanner sc = null;
+        File privateKeyFile = new File(args[2]);
+        try{
+            sc = new Scanner(privateKeyFile);
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+            System.err.printf("Private key not found, please try again: %s.\n", args[2]);
+            argsError();
+        }
+
+        //convert to big integer
+        String exponentStr = sc.nextLine();
+        String modStr = sc.nextLine();
+
+        BigInteger exponent = new BigInteger(exponentStr);
+        BigInteger mod      = new BigInteger(modStr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        FileInputStream
+
+
 
     }
     public static void argsError(){
