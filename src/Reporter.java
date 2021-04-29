e2fd70ccaa69355151b141d5917119eb35795615
Navid Nafiuzzaman <mxn4459@rit.edu>
2021-04-29 06:24:45 -0400

    v1 working reporter

--- src/Reporter.java
+++ src/Reporter.java
@@ -1,32 +1,64 @@
+//******************************************************************************
+//
+// File:    Reporter.java
+// Package: edu.rit.pj2
+// Unit:    Class Reporter
+//
+// Copyright (c) 2021 - 2022 Navid Nafiuzzaman
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// This Java source file is part of the Parallel Java 2 Library ("PJ2"). PJ2 is
+// free software; you can redistribute it and/or modify it under the terms of
+// the GNU General Public License as published by the Free Software Foundation;
+// either version 3 of the License, or (at your option) any later version.
+//******************************************************************************
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
 import java.util.Scanner;
+
 
 public class Reporter {
     public static void main(String[] args) throws SocketException {
         if (args.length < 3 ) argsError();
 
         String rHost = args[0];
-
         int rPort = Integer.parseInt(args[1]);
         DatagramSocket mailbox = null;
-
 
         try{
             mailbox = new DatagramSocket(new InetSocketAddress(rHost, rPort));
         }catch (SocketException e){
             e.printStackTrace();
-            System.err.printf("Error in accessing the specific address %s:%d", rHost, rPort);
+            System.err.printf("Error in accessing the specific address %s:%d \n", rHost, rPort);
             argsError();
             System.exit(1);
         }
 
-
+        // handling privatekeyFile
         Scanner sc = null;
         File privateKeyFile = new File(args[2]);
         try{
@@ -38,33 +70,48 @@ public class Reporter {
         }
 
         //convert to big integer
-        String exponentStr = sc.nextLine();
-        String modStr = sc.nextLine();
+        String dStr = sc.nextLine();
+        String nStr = sc.nextLine();
+        OAEP oaep = new OAEP();
 
-        BigInteger exponent = new BigInteger(exponentStr);
-        BigInteger mod      = new BigInteger(modStr);
+        BigInteger d = new BigInteger(dStr);
+        BigInteger n = new BigInteger(nStr);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//        FileInputStream
+        byte[] buffer = new byte[260];
+        BigInteger incomingMsg;
+        while (true){
+            try{
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                mailbox.receive(packet);
+//            TODO: Figure out the incoming buffer
+                byte[] incomingBuffer = packet.getData();
+                BigInteger msg = new BigInteger(incomingBuffer, 0, packet.getLength());
+                String output = decryption(d, n, msg, oaep);
+                System.out.println(output);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
 
+    }
 
+
+    private static String decryption(BigInteger d,
+                                     BigInteger n,
+                                     BigInteger msg,
+                                     OAEP oaep){
+//        TODO: understand the decryption process
+        byte[] seed = new byte[32];     // 32 from OAEP size
+        try{
+            String decodedMsg = oaep.decode(msg.modPow(d, n));
+            return decodedMsg;
+        } catch (IllegalArgumentException e){
+            e.printStackTrace();
+            System.err.println("Private key cannot decrypt the message sent using public key");
+            System.exit(1);
+        }
+        return "";
     }
     public static void argsError(){
         System.err.println("Usage: java Reporter <rhost> <rport> <privatekeyfile>");
